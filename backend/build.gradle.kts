24192f8df19a9d41c2bbee088b5171e24bf07dc2
flo <boireaufloriane@hotmail.fr>
2025-04-23 11:49:04 +0200

    test fonctionnel pour postservice et userservice

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -18,20 +18,26 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-rest:3.4.4")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.4.4")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("org.postgresql:postgresql:42.7.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.4")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.7.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("me.paulschwarz:spring-dotenv:4.0.0")
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.7.1")
-    testImplementation ("org.mockito:mockito-junit-jupiter:3.11.2")
-    testImplementation ("org.mockito:mockito-core:3.11.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+    testImplementation ("org.mockito:mockito-core")
 }
 tasks.named<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun") {
     jvmArgs = listOf("--add-opens=java.base/java.io=ALL-UNNAMED")
 }
 
-
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true // Montre les println
+    }
+}
