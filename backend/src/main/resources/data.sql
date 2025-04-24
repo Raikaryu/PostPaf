674881438469bdbc65d3ff49a937db13c96d9479
Raika <pejuvenelle@hotmail.com>
2025-04-24 17:10:44 +0200

    Ajout des photos

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO "users" ("id", "pseudo", "email", "password", "bio", "image", "creationdate") VALUES
-(1, 'pierre', 'pierre@example.com', 'securepassword1', 'Passionné de technologie et de développement.', 'https://raw.githubusercontent.com/Raikaryu/PostPaf/refs/heads/dev/frontend/public/assets/pierre.png', '2023-01-15 10:00:00'),
-(2, 'floriane', 'floriane@example.com', 'securepassword2', 'Amoureuse de la nature et de la photographie.', 'https://raw.githubusercontent.com/Raikaryu/PostPaf/refs/heads/dev/frontend/public/assets/floriane.png', '2023-02-20 14:30:00'),
-(3, 'sarra', 'sarra@example.com', 'securepassword3', 'Explorateur de nouvelles technologies.', 'https://raw.githubusercontent.com/Raikaryu/PostPaf/refs/heads/dev/frontend/public/assets/sarra.png', '2023-03-10 09:45:00');
+(1, 'Seli', 'seli@example.com', 'securepassword1', 'Epouse de Léodagan.', 'https://raw.githubusercontent.com/Raikaryu/PostPaf/refs/heads/dev/frontend/public/assets/seli.jpg', '2023-01-15 10:00:00'),
+(2, 'Arthur', 'arthour@example.com', 'securepassword2', 'Roi de Bretagne, ancien romain devenu chef des rois fédérés de Bretagne. Choisi par les dieux pour mener la quête du Graal.', 'https://raw.githubusercontent.com/Raikaryu/PostPaf/refs/heads/dev/frontend/public/assets/arthur.jpg', '2023-02-20 14:30:00'),
+(3, 'Léodagan', 'dagan@example.com', 'securepassword3', 'Roi de Carmélide, beau-père d''Arthur et spécialiste de la fortification. Amateur de justice expéditive.', 'https://raw.githubusercontent.com/Raikaryu/PostPaf/refs/heads/dev/frontend/public/assets/leodagan.jpg', '2023-03-10 09:45:00');
 
 INSERT INTO "post" ("id", "id_user", "title", "contenu", "creationdate") VALUES
-(1, 1, 'Introduction à Spring Boot', 'Spring Boot est un framework puissant pour développer des applications Java.', '2023-01-16 11:00:00'),
-(2, 2, 'Les Meilleures Pratiques en Photographie', 'Découvrez les astuces pour capturer des photos magnifiques.', '2023-02-21 15:30:00'),
-(3, 3, 'Les Tendances Tech de 2023', 'Explorons les innovations technologiques de cette année.', '2023-03-11 10:45:00');
+(1, 1, 'Les tartes', 'Non mais je vais varier les fruits, vous inquiétez pas.', '2023-01-16 11:00:00'),
+(2, 2, 'Les tartes', 'Et vous allez varier la pâte aussi ?.', '2023-01-16 11:02:00'),
+(3, 1, 'Les tartes', 'Non mais n''exagérez pas non plus, je vous demande quand même pas de manger des briques.', '2023-01-16 11:03:00');
